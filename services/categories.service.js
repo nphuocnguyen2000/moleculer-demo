b1ad5574961ec9ef1a8ff29395b154b4e1d6203d
Le Phuoc Son <l.phuocson.it@gmail.com>
2020-10-28 11:27:54 +0700

    update categories

--- services/categories.service.js
+++ services/categories.service.js
@@ -1,21 +1,45 @@
 "use strict";
-
+const { MoleculerClientError } = require("moleculer").Errors;
+const { ForbiddenError }       = require("moleculer-web").Errors;
+const DbService                = require("moleculer-db");
+const MongooseAdapter          = require("moleculer-db-adapter-mongoose");
+const CATEGORY_COLL            = require("../models/category_coll"); 
 /**
  * categories service
  */
 module.exports = {
 
 	name: "categories",
-
+	mixins: [DbService],
+	adapter: new MongooseAdapter(process.env.MONGO_URI || "mongodb://localhost:27017/moleculer-test", 
+		{ 
+			useNewUrlParser: true, 
+			useUnifiedTopology: true 
+		}
+	),
+	model: CATEGORY_COLL,
 	/**
 	 * Service settings
 	 */
-	settings: {},
+	settings: {
+		fields: ["_id", "name", "products"],
+		// populates : {
+		// 	"products": {
+		// 		action: "users.get",
+		// 		params: ["_id"]
+		// 	}
+		// }
+		entityValidator: {
+			name: { type: "string"}
+		}
+	},
 
 	/**
 	 * Service metadata
 	 */
-	metadata: {},
+	metadata: {
+
+	},
 
 	/**
 	 * Service dependencies
@@ -29,9 +53,30 @@ module.exports = {
 		/**
 		* Test action
 		*/
-		test: {
-			async handler(ctx) {
-				return "Hello Moleculer";
+		// test: {
+		// 	async handler(ctx) {
+		// 		return "Hello Moleculer";
+		// 	}
+		// }
+		create: {
+			rest: "POST /",
+			params: {
+				name: "string"
+			},
+			async handler(ctx){
+				console.log(ctx.params);
+				if(!ctx.params){
+					throw new MoleculerClientError("Invalid params!", 404, "INVALID_PARAMS");
+				}
+
+				let nameCategory = await this.adapter.find
+
+				let infoCategory = await this.adapter.insert(ctx.params);
+				if(!infoCategory){
+					throw new MoleculerClientError("Invalid params!", 404, "INVALID_PARAMS");
+				}
+				console.log({infoCategory});
+				return this.transformDocuments(ctx, {}, infoCategory);
 			}
 		}
 	},
